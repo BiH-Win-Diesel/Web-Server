347cbb3e72924b784a841ebcb48969c384dca32a
Subhojit Dey <subhojit2000dey@gmail.com>
2024-01-13 22:42:16 +0530

    Login Authentication and middleware

--- app/page.js
+++ app/page.js
@@ -1,8 +1,22 @@
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route.js"
 
-export default function Home() {
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
   return (
     <>
-    <h1>Heading</h1>
+    <div className='grid grid-cols-2 text-white p-4'>
+      <div>
+        {
+          session !== null
+            ? <h1 className='leading-loose text-[15rem] font-extrabold text-accent'>
+                Hi {session?.user.name}!
+              </h1>
+            : <a className='btn btn-primary' href='/api/auth/signin'>Sign in</a>
+        }
+      </div>
+    </div>
     </>
   )
 }
