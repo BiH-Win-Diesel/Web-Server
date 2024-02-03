e21fc2f92079279fc7e8d37c06aca433557aba28
Soumya Tarafder <63846042+Soumya624@users.noreply.github.com>
2024-02-03 23:32:07 +0530

    added (#8)

--- app/dashboard/components/ImageModal.js
+++ app/dashboard/components/ImageModal.js
@@ -7,35 +7,51 @@ import {
   DialogTitle,
   TextField,
 } from "@material-ui/core";
-import { useFileUpload } from "@/app/hooks/lib/uploadImage";
 
-const ProductModal = ({ open, handleClose, handleSave }) => {
+const ImageModal = ({ open, handleClose, handleSave }) => {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState("");
   const [price, setPrice] = useState("");
   const [imageSourceLink, setImageSourceLink] = useState("");
-  const [disableSave, setDisableSave] = useState(true);
+  const [productName, setProductName] = useState("");
 
-  const handleSaveClick = () => {
+  const fetchProductName = async () => {
+    const requestData = {
+      imageSourceLink: imageSourceLink,
+    };
+
+    try {
+      const response = await fetch('http://localhost:3001/detect-text', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+      console.log(result);
+      setProductName(result.productName);
+    } catch (error) {
+      console.error('Error fetching product name:', error);
+    }
+  };
+  const handleSaveClick = async () => {
+    await fetchProductName();
+
     handleSave({
-      data: description,
+      productName,
       description,
       quantity: parseInt(quantity),
       price: parseFloat(price),
       imageSourceLink,
     });
+
     handleClose();
-  };
-
-  const uploadFile = useFileUpload()
-
-  const handleFileSelect = async (file) => {
-    const uploadOk = await uploadFile(file.name, file);
-    if(uploadOk){
-      alert("Image Upload Successfully....");
-      setImageSourceLink(file.name)
-      disableSave(true)
-    }
   };
 
   return (
@@ -47,14 +63,13 @@ const ProductModal = ({ open, handleClose, handleSave }) => {
       <DialogTitle id="form-dialog-title">Add Product</DialogTitle>
       <DialogContent>
         <TextField
-          autoFocus
           margin="dense"
-          id="description"
-          label="Product *"
+          id="imageSourceLink"
+          label="Image Source Link *"
           type="text"
           fullWidth
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          value={imageSourceLink}
+          onChange={(e) => setImageSourceLink(e.target.value)}
         />
         <TextField
           margin="dense"
@@ -64,19 +79,6 @@ const ProductModal = ({ open, handleClose, handleSave }) => {
           fullWidth
           value={price}
           onChange={(e) => setPrice(e.target.value)}
-        />
-        <TextField
-          margin="dense"
-          id="imageSourceLink"
-          label="Image Source Link *"
-          type="text"
-          fullWidth
-          value={imageSourceLink}
-          disabled={true}
-        />
-        <input
-          type="file"
-          onChange={(e) => handleFileSelect(e.target.files[0])}
         />
         <TextField
           margin="dense"
@@ -92,7 +94,7 @@ const ProductModal = ({ open, handleClose, handleSave }) => {
         <Button onClick={handleClose} color="primary">
           Cancel
         </Button>
-        <Button onClick={handleSaveClick} disabled={disableSave} color="primary">
+        <Button onClick={handleSaveClick} color="primary">
           Save
         </Button>
       </DialogActions>
@@ -100,4 +102,4 @@ const ProductModal = ({ open, handleClose, handleSave }) => {
   );
 };
 
-export default ProductModal;
+export default ImageModal;
